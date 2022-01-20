491d0462446f6b32a482ce6a9f4705b102e45d0d
JeonSoohyun <jeonsoohyun27@gmail.com>
2022-01-20 16:47:06 +0900

    Create gitignore

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from typing import Sequence
+from django.shortcuts            import render
+from django.views.generic.list   import ListView
+from django.views.generic.edit   import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models                     import photo
+class PhotoList(ListView):
+    model = photo
+    template_name_suffix = '_list'
 
-# Create your views here.
+class PhotoCreate(CreateView):
+    model = photo
+    fields = ['text','image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+class PhotoUpdate(UpdateView):
+    model = photo
+    fields = ['text','image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+class PhotoDelete(DeleteView):
+    model = photo
+    fields = ['text','image']
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+class PhotoDetail(DetailView):
+    model = photo
+    fields = ['text','image']
+    template_name_suffix = '_detail'
+    success_url = '/'
